94d348281f917b19dd9aeec1d7264406e53f50bb
Sahnawaz Hussain <sahnawazhussain852@gmail.com>
2023-04-10 04:13:49 +0530

    updated

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-//const base_url = process.env.MONGO_URL;
+const base_url = process.env.MONGO_URL;
 
-console.log("MONGO_URL: ", base_url);
+//console.log("MONGO_URL: ", base_url);
 
 const connectDatabase = mongoose.connect(base_url);
 
